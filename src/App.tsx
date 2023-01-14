8f25b579c93438061d2a6f79cf78d17fcc99c127
Muzafar-Ali <muzafar.ali.chandio@gmail.com>
2023-01-15 03:24:35 +0500

    initial commit
    add

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import {Box} from '@chakra-ui/react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Box bg='gray.300' h='200px' w='200px'>
+        This is chakra UI Box
+      </Box>
+    </div>
+  )
+}
+
+export default App
